8974f531c59db19514d3b86c893fe2824226a65e
Andrei Roditelev <rodandr13@gmail.com>
2022-01-29 19:13:05 +0530

    добавил проект yatube

---
+++ yatube/posts/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+
+from .models import Post
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('text', 'group')
+
+    def clean_text(self):
+        data = self.cleaned_data['text']
+        if not data:
+            raise forms.ValidationError('Введите текст поста')
+        return data
+
+
+
+
---
+++ yatube/posts/views.py
@@ -0,0 +1,108 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+
+from .models import Post, Group, User
+from .forms import PostForm
+
+COUNT_ELEMS = 10
+
+
+def index(request):
+    template = 'posts/index.html'
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, COUNT_ELEMS)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'page_obj': page_obj
+    }
+    return render(request, template, context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    post_list = group.posts.all()
+    paginator = Paginator(post_list, COUNT_ELEMS)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    template = 'posts/group_list.html'
+    context = {
+        'group': group,
+        'page_obj': page_obj,
+    }
+    return render(request, template, context)
+
+
+def profile(request, username):
+    template = 'posts/profile.html'
+    author = User.objects.get(username=username)
+    post_list = author.posts.all()
+    count_posts = post_list.count()
+    paginator = Paginator(post_list, COUNT_ELEMS)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'page_obj': page_obj,
+        'count_posts': count_posts,
+        'author': author,
+    }
+    return render(request, template, context)
+
+
+def post_detail(request, post_id):
+    template = 'posts/post_detail.html'
+    post = Post.objects.get(pk=post_id)
+    author = User.objects.get(username=post.author)
+    count_posts = author.posts.count()
+    print(type(post))
+    context = {
+        'post': post,
+        'count_posts': count_posts,
+    }
+    return render(request, template, context)
+
+
+@login_required
+def post_create(request):
+    template = 'posts/create_post.html'
+    form = PostForm()
+    context = {
+        'form': form
+    }
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data['text']
+            group = form.cleaned_data['group']
+            new_post = form.save(commit=False)
+            new_post.author = request.user
+            form.save()
+            return redirect(f'/profile/{request.user}/')
+        return render(request, template, context)
+    return render(request, template, context)
+
+
+@login_required
+def post_edit(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    template = 'posts/create_post.html'
+    context = {
+        'post': post
+    }
+    if request.user != post.author:
+        return redirect('posts:post_detail', post_id=post.id)
+    if request.method == 'GET':
+        is_edit = True
+        form = PostForm(instance=post)
+        context = {
+            'post': post,
+            'form': form,
+            'is_edit': is_edit,
+        }
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+        return redirect('posts:post_detail', post_id=post_id)
+    return render(request, template, context)
