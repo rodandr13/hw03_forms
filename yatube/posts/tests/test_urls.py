53b9ee27dd9fdc38f57ed887bd88110a6b228e6a
Andrei Roditelev <rodandr13@gmail.com>
2022-02-07 21:15:36 +0530

    написал тесты для URLs

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -1,22 +1,71 @@
+from http import HTTPStatus
+
 from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.core.cache import cache
+
+from ..models import Post, Group
+
+User = get_user_model()
 
 
-class StaticURLTests(TestCase):
+class StaticPageURLTesting(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = User.objects.create_user(username='auth', )
+        cls.group = Group.objects.create(
+            title='Заголовок тестовой группы',
+            slug='test-slug',
+            description='Тестовое описание',
+        )
+        cls.post = Post.objects.create(
+            author=cls.user,
+            text='Тестовая группа',
+        )
+
     def setUp(self):
-        # Устанавливаем данные для тестирования
-        # Создаём экземпляр клиента. Он неавторизован.
         self.guest_client = Client()
+        self.user = StaticPageURLTesting.user
+        self.authorized_client = Client()
+        self.authorized_client.force_login(self.user)
+        cache.clear()
 
-    def test_homepage(self):
-        # Отправляем запрос через client,
-        # созданный в setUp()
-        response = self.guest_client.get('/')
-        self.assertEqual(response.status_code, 200)
+    def test_urls_uses_correct_template_guest_users(self):
+        templates_url_names = {
+            'posts/index.html': '/',
+            'posts/group_list.html': '/group/test-slug/',
+            'posts/profile.html': '/profile/auth/',
+            'posts/post_detail.html': '/posts/1/',
+        }
+        for template, address in templates_url_names.items():
+            with self.subTest(address=address):
+                response = self.guest_client.get(address)
+                self.assertTemplateUsed(response, template)
 
-    def test_author(self):
-        response = self.guest_client.get('/about/author/')
-        self.assertEqual(response.status_code, 200)
+    def test_urls_uses_correct_template_auth_user(self):
+        response = self.authorized_client.get('/create/')
+        self.assertTemplateUsed(response, 'posts/create_post.html')
 
-    def test_tech(self):
-        response = self.guest_client.get('/about/tech/')
-        self.assertEqual(response.status_code, 200)
+    def test_urls_uses_correct_template_edit_author(self):
+        author = StaticPageURLTesting.post.author
+        authorized_client = self.user
+        self.assertEqual(author, authorized_client)
+
+    def test_urls_exists_at_desired_location(self):
+        url_status_codes = {
+            '/': 200,
+            '/group/test-slug/': 200,
+            '/profile/auth/': 200,
+            '/posts/1/': 200,
+            '/posts/1/edit/': 302,
+            '/create/': 302,
+        }
+        for url, code in url_status_codes.items():
+            with self.subTest(url=url):
+                response = self.guest_client.get(url)
+                self.assertEqual(response.status_code, code)
+
+    def test_server_responds_404_unexisted_page(self):
+        response = self.authorized_client.get('/unixisted-page/')
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
